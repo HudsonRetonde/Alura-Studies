bac66e74f76ebc669c882c206007ff3905696adf
Hudson Retonde <hudsonsretonde@gmail.com>
2022-08-30 23:12:14 -0300

    setTarefas.

--- alura-studies/src/componentes/Lista/index.tsx
+++ alura-studies/src/componentes/Lista/index.tsx
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 import style from './Lista.module.scss';
 
 function Lista () {
-    let tarefas = [{
-        tarefa: 'React',
-        tempo: '02:00:00'
-    }, {
-        tarefa: 'JavaScript',
-        tempo: '01:00:00'
-    }, {
-        tarefa:'TypeScript',
-        tempo: '03:00:00'
-    }]
+    const [tarefas, setTarefas] = useState (
+        [{
+            tarefa: 'React',
+            tempo: '02:00:00'
+        }, {
+            tarefa: 'JavaScript',
+            tempo: '01:00:00'
+        }, {
+            tarefa:'TypeScript',
+            tempo: '03:00:00'
+        }]
+    );
     return (
         <aside className={style.listaTarefas}>
             <h2 onClick={()=> {
-                tarefas = [...tarefas, {tarefa: "Estudar Estado", tempo: "05:00:00"}]
+                setTarefas([...tarefas, {tarefa: "Estudar Estado", tempo: "05:00:00"}])
             }}>Estudos do Dia</h2>
             <ul>
                 {tarefas.map((item, index) =>
